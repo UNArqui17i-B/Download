867de4754dd04affd1b10888827789cd0167f25c
Laura Alejandra Chaparro Gutiérrez <lauralejandracg@gmail.com>
2017-03-15 18:28:54 -0500

    Verify database connection

--- download.go
+++ download.go
@@ -4,6 +4,7 @@ import (
 		"github.com/ant0ine/go-json-rest/rest"
 		"log"
 		"net/http"
+		"fmt"
 )
 
 type RequestedFile struct{
@@ -21,6 +22,38 @@ func ReceiveRequest(w rest.ResponseWriter, req *rest.Request) {
 	w.WriteJson(&fileRequest)
 }
 
+func VerifyDatabaseExistance(url string) {
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		log.Fatal("Database connection request: ", err)
+		return	
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Database connection do: ", err)
+	}
+
+	fmt.Printf("Database connection: %s\n", resp.Status)
+
+	if resp.StatusCode == 404 {
+		req, err = http.NewRequest("PUT", url, nil)
+		if err != nil {
+			log.Fatal("Database creation request: ", err)
+			return
+		}
+
+		resp, err = client.Do(req)
+		if err != nil {
+			log.Fatal("Database creation do: ", err)
+		}
+
+		fmt.Printf("Database creation: %s\n", resp.Status)
+	}
+}
+
 func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
@@ -31,6 +64,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	VerifyDatabaseExistance("http://127.0.0.1:5984/blinkbox_files")
 
 	api.SetApp(router)
 	log.Fatal(http.ListenAndServe(":4025", api.MakeHandler()))
