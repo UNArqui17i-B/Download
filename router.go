c34e346523a90029ae62a83302fbc3a1a9aec8da
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-04-12 15:20:55 -0500

    Feature/shared with (#14)
    * Retrieve the IDs of the files that have been shared with the user
    
    * Retrieve the Document Information instead of just the ID

--- router.go
+++ router.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"github.com/gorilla/mux"
 	"fileAccess/access"
 	"net/http"
@@ -15,6 +15,7 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/info/{id}", access.GetInformation)
 	router.HandleFunc("/download/{id}/{email}", access.Download)
+	router.HandleFunc("/sharedWith/{email}", access.FilesSharedWith)
 
 	router.NotFoundHandler = http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(http.StatusNotFound)
